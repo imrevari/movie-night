fa5d7b3e8e1d1b4fb9e4f08de3c9ff3698ba2add
Imre Vari <imrevari2@gmail.com>
2023-11-21 17:01:06 -0500

    server logic working properly

--- src/server/constants.js
+++ src/server/constants.js
@@ -4,7 +4,9 @@ const API_KEY = 'b6efcaaa9567ac730fb92e4d61424564'
 const EXTERNAL_API_URL = 'https://api.themoviedb.org/3/search/'
 const MOVIE_PARAM = 'movie?query='
 const END_PARAMS = `&include_adult=false&language=en-US&page=1&api_key=${API_KEY}`
+const LOOKUP_TIME_SECONDS = 120
 
 exports.EXTERNAL_API_URL = EXTERNAL_API_URL
 exports.MOVIE_PARAM = MOVIE_PARAM
 exports.END_PARAMS = END_PARAMS
+exports.LOOKUP_TIME_SECONDS = LOOKUP_TIME_SECONDS
--- src/server/movieRouts.js
+++ src/server/movieRouts.js
@@ -2,45 +2,50 @@ const express = require('express')
 const app = express()
 var axios = require('axios')
 
-const movies = require('./movies.json') 
 const router = express.Router()
-const {EXTERNAL_API_URL, MOVIE_PARAM, END_PARAMS} = require('./constants')
-
-const {results} = movies
-
-
-let localMovies = [...results]
+const {EXTERNAL_API_URL, MOVIE_PARAM, END_PARAMS, LOOKUP_TIME_SECONDS} = require('./constants')
 
 const mapOfWords = new Map();
+let cacheHit = 0;
 
-const fetchNewRequest = (movieName) => {
-    axios.get(EXTERNAL_API_URL.concat(MOVIE_PARAM).concat(movieName).concat(END_PARAMS))
+const fetchNewRequest = async (movieName) => {
+    const movies = await axios.get(EXTERNAL_API_URL.concat(MOVIE_PARAM).concat(movieName).concat(END_PARAMS))
     .then(res => {
       const results = res.data.results;
-    console.log(results)
+      return results;
     }
-      ).catch( error => console.log(error));
+      ).catch( error => console.log(error))
+    return movies;
+}
+
+
+const getListOfMovies = async (movieName) => {
+    if(mapOfWords.get(movieName)){
+        const now = new Date();
+        const {fetched, movies} = mapOfWords.get(movieName);
+        const timeDifference = now.getTime() - fetched.getTime();
+        if(timeDifference < (LOOKUP_TIME_SECONDS * 1000)){
+            cacheHit++;
+            return movies;
+        }
+    }
+    const movies = await fetchNewRequest(movieName)
+        mapOfWords.set(movieName, {
+            fetched: new Date(),
+            movies: movies
+        })
+        cacheHit = 0;
+        return movies;
 }
 
 
 
 
-router.get('/:name', (req, res) => {
+router.get('/:name', async (req, res) => {
     const searchName = req.params.name;
-    console.log(searchName)
-    fetchNewRequest(searchName)
-    res.send(localMovies)
+    const movies = await getListOfMovies(searchName)
+    res.send({cacheHit: cacheHit, movies: movies})
 })
 
-
-
-router.post('/', (req, res) => {
-})
-
-router.put('/:id', (req, res) => {
-})
-
-router.delete('/:id', (req, res) => {
-})
 
 module.exports = router
