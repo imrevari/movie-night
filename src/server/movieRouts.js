79ba76f529db3864d360141c873f872517b61b46
Imre Vari <imrevari2@gmail.com>
2023-11-21 18:58:43 -0500

    error handeling added

--- src/server/movieRouts.js
+++ src/server/movieRouts.js
@@ -16,7 +16,8 @@ const fetchNewRequest = async (movieName, page) => {
       const results = res.data.results;
       return results;
     }
-      ).catch( error => console.log(error))
+      ).catch( error => {
+        return error})
     return movies;
 }
 
@@ -32,6 +33,13 @@ const getListOfMovies = async (movieName, page) => {
         }
     }
     const movies = await fetchNewRequest(movieName, page)
+    if(movies?.response){
+        return({
+            error: true,
+            status: movies.response.status,
+            message: movies.response.data.status_message
+        })
+    }
         mapOfWords.set(movieName.concat(page), {
             fetched: new Date(),
             movies: movies
@@ -46,11 +54,17 @@ const getListOfMovies = async (movieName, page) => {
 router.get('/movie', async (req, res) => {
     const searchName = req.query.name;
     if (!searchName) {
-        return res.status(404).json({ message: 'No Search name provided' })
+        return res.status(400).json({ message: 'No Search name provided' })
       }
 
     const page = req.query.page ?? 1;
     const movies = await getListOfMovies(searchName, page);
+
+    if(movies.error){
+        const {status, message} = movies;
+        return res.status(status).json({ message: message })
+    }
+
     res.send({cacheHit: cacheHit, movies: movies});
 })
 
