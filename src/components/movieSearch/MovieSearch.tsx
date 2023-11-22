db0f2b593d0ac6d6b7b822cf93cb77b982968118
Imre Vari <imrevari2@gmail.com>
2023-11-22 17:07:06 -0500

    business logic working properly w/o validation

--- src/components/movieSearch/MovieSearch.tsx
+++ src/components/movieSearch/MovieSearch.tsx
@@ -1,47 +1,59 @@
-import { Box, Chip, Container, Pagination, Typography } from "@mui/material";
-import { FC, useMemo, useState } from "react";
-import SearchBar from "./SearchBar";
+import { Box, Chip, Container } from "@mui/material";
+import { FC, useState } from "react";
 import MovieTile from "./MovieTile";
+import SearchBar from "./SearchBar";
 
-import movie from '../../server/movies.json'
-import Paginator from "./Paginator";
-import { Movie } from "../../interfaces/interfaces";
 import axios from "axios";
-import { LOCAL_BACKEND } from "../../constants/constants";
+import { LOCAL_BACKEND, NAME_PARAM, PAGE_PARAM } from "../../constants/constants";
+import { Movie } from "../../interfaces/interfaces";
+import Paginator from "./Paginator";
 
 
 
 const MovieSearch: FC = () => {
 
-    // const movies = movie.results;
     const movies = new Array<Movie>;
 
     const [fetchedMovies, setFetchedMovies] = useState<Movie[]>([])
     const [page, setPage] = useState<number>(1)
+    const [searchPhrase, setSearchPhrase] = useState<string>('')
     const [cashedFetches, setCashedFetches] = useState<number>(0)
     const [totalPages, setTotalPages] = useState<number>(1)
     const [error, setError] = useState<string | null>(null)
 
-    const moviesToDisplay = useMemo(() => {
-
-    }, [page, fetchedMovies, totalPages])
-
-
-    const fetchFromServer = (movieTitle: string) =>{
+    const fetchFromServer = (movieTitle: string, pageParam: number) =>{
         axios.get(
-            LOCAL_BACKEND.concat(movieTitle)
+            LOCAL_BACKEND.concat(NAME_PARAM).concat(movieTitle).concat(PAGE_PARAM).concat(pageParam.toString())
         )
         .then(res => {
+            const responseBody = res.data;
+            const {cacheHit, totalPages, movies} = responseBody;
 
-            console.log(res)
+            setFetchedMovies(movies)
+            setCashedFetches(cacheHit)
+            setTotalPages(totalPages)
+
         }
-          ).catch( error => console.log(error))
+          ).catch( error => console.log(error.response.data))
+    }
+
+    const triggerSearch = (movieTitle: string) => {
+        setPage(1)
+        setSearchPhrase(movieTitle)
+        fetchFromServer(movieTitle, 1);
+    }
+
+    const triggerPaginator = (selectedPage: number) =>{
+        if(selectedPage != page){
+            setPage(selectedPage)
+            fetchFromServer(searchPhrase, selectedPage);
+        }  
     }
     
 
     return(
         <Container >
-            <SearchBar triggerSearch={((e: any) => console.log(e))}/>
+            <SearchBar triggerSearch={triggerSearch}/>
             <br></br>
             <Box sx={{backgroundColor: '#e9f1f2',
                         display: 'flex',
@@ -51,16 +63,16 @@ const MovieSearch: FC = () => {
                         justifyContent: 'center'}}>
                 
                 <Chip 
-                    label={'Text goes here from API'} 
+                    label={`Text goes here from API ${cashedFetches}`} 
                     // variant="outlined"
                     color={true ? 'default' : 'error'}
                     sx={{marginTop: '20px', marginBottom: '10px', minWidth: '65%', }}/>
 
                 <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center'}}>
-                    {movies.map( (movie) => <MovieTile movie={movie}/>)} 
+                    {fetchedMovies.map( (movie) => <MovieTile movie={movie}/>)} 
                 </Box>
 
-                <Paginator pages={totalPages} setPage={setPage}/>
+                <Paginator pages={totalPages} setPage={triggerPaginator} curentPage={page}/>
             </Box>
         </Container>
     )
--- src/components/movieSearch/Paginator.tsx
+++ src/components/movieSearch/Paginator.tsx
@@ -3,21 +3,16 @@ import { FC, useState } from "react";
 
 
 
-const Paginator: FC<any> = ({pages, setPage}) => {
+const Paginator: FC<any> = ({pages, setPage, curentPage}) => {
 
-    const [curentPage, setCurrentPage] = useState<number>(1)
 
-    const changePage =(newPage: number) => {
-        setCurrentPage(newPage)
-        setPage(newPage)
-    }
 
     return(
         <Pagination 
         count={pages} 
         sx={{ margin: '15px 0 30px 0'}} 
         page={curentPage} 
-        onChange={(_, page) => changePage(page)}
+        onChange={(_, page) => setPage(page)}
         size="large"/>
     )
 
